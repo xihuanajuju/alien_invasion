de02c1465412536a1d4ffca5508925358ddc8062
xihuanajuju <1326351413@qq.com>
2022-06-04 20:34:35 +0800

    重构方法run_game，练习12.2创建linus类

--- ailen_invasion.py
+++ ailen_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from linus import Linus
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
@@ -15,21 +16,27 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
+        self.linus = Linus(self)
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            #监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            #每次循环时都会重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._check_events()
+            self._update_screen()
 
-            #让最近绘制的屏幕可见
-            pygame.display.flip()
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        self.linus.blitme()
+        
+        pygame.display.flip()
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏
---
+++ linus.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Linus:
+    """管理linus的类"""
+
+    def __init__(self, ai_game) -> None:
+        """加载linus的图片并设置其初始位置"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #加载linus图片并获取其矩形
+        self.image = pygame.image.load("./images/linus.bmp")
+        self.rect = self.image.get_rect()
+
+        #将linus放置到屏幕正中央
+        self.rect.center = self.screen_rect.center
+    
+    def blitme(self):
+        """在指定位置绘制linus"""
+        self.screen.blit(self.image, self.rect)
